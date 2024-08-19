257004f26ba3631b7179ec5a37291abc2765427c
starshipunicorn <166185024+starshipunicorn@users.noreply.github.com>
2024-08-19 03:06:53 -0400

    Update servicing.js

--- servicing.js
+++ servicing.js
@@ -17,7 +17,7 @@ const upchargeRates = {
     "super": 0.25
 };
 
-const webhookURL = "YOUR_DISCORD_WEBHOOK_URL";
+const webhookURL = "https://discord.com/api/webhooks/1274987812651012146/jCsgYjqv7rp4hCdoi5O-IsCXzRDCFeIruIMZ4zl24OtrsFLQLzHeU36qLQtJrqHoitUy";
 let lastServicingMessage = "";  // To store the last servicing message
 let totalServicingCost = 0;  // Total cost of selected servicing parts
 let totalServicingCostWithUpcharge = 0;  // Total cost with upcharge
